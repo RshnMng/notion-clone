0375bb04198d39f1f624f5db540def2edf1b6be4
RshnMng <155337224+RshnMng@users.noreply.github.com>
2025-04-30 10:27:39 -0400

    building header with clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
       <main>
         <h1>shit be workin'</h1>
+        <Button>Click me </Button>
       </main>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React from 'react'
+import { useUser, SignedIn, SignedOut, UserButton, SignInButton } from '@clerk/nextjs';   
+
+const Header = () => {
+    const { user } = useUser();
+  return (
+    <div className='bg-teal-500 flex items-center justify-between p-5'>
+        {user && <h1 className='border border-red-500'>{user?.firstName}{`'s`} Space</h1>}
+
+                 <div>
+                      <SignedOut>
+                                <SignInButton />
+                      </SignedOut>
+                      <SignedIn>
+                                <div className='border border-blue-500'>
+                                <UserButton/>
+                                </div>
+                      </SignedIn>
+                 </div>
+    </div>
+  )
+}
+
+export default Header
