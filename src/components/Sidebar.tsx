3c394fe4228bbef27dedad3185e37fb649813b33
RshnMng <155337224+RshnMng@users.noreply.github.com>
2025-04-30 11:17:46 -0400

    added a sheet for the sidebar with shadcn

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import NewDocumentButton from './NewDocumentButton'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 
 const Sidebar = () => {
   return (
     <div className='p-2 md:p-5 relative'>
-                 <NewDocumentButton />
+        <Sheet>
+            <SheetTrigger>Open</SheetTrigger>
+            <SheetContent side='left' className="w-[400px] sm:w-[540px]">
+             <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+                 <SheetDescription>
+                  This action cannot be undone. This will permanently delete your account
+                  and remove your data from our servers.
+               </SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+
+                <div className='hidden md:inline'>
+                        <NewDocumentButton />
+
+                </div>
     </div>
   )
 }
