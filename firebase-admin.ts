54378487cfb74f9fa90460d6beda0eee380c7608
RshnMng <155337224+RshnMng@users.noreply.github.com>
2025-05-02 12:10:51 -0400

    got the new document button hooked up to firebase

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -8,7 +8,9 @@ import {
 
 import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceKey = require("@/service_key.json");
+import serviceKey from './service_key.json';
+
+
 
 let app: App;
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,13 @@
 import React from 'react'
 import { useUser, SignedIn, SignedOut, UserButton, SignInButton } from '@clerk/nextjs';   
 
+
 const Header = () => {
     const { user } = useUser();
+    
   return (
     <div className='bg-teal-500 flex items-center justify-between p-5'>
         {user && <h1 className='border border-red-500 text-2xl'>{user?.firstName}{`'s`} Space</h1>}
-
                  <div>
                       <SignedOut>
                                 <SignInButton />
