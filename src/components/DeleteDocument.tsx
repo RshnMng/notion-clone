93f8e05853985588f34ee9942432bdd43f2be39b
rashaan <rmng.developer@gmail.com>
2025-05-25 16:31:07 -0400

    setting up destructive delete button for owner of document

--- src/components/DeleteDocument.tsx
+++ src/components/DeleteDocument.tsx
@@ -1,6 +1,8 @@
 'use client';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
+import { usePathname } from 'next/navigation';
+
 import {
   Dialog,
   DialogContent,
@@ -8,24 +10,47 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogFooter,
+  DialogClose
 } from "@/components/ui/dialog"
+import { Button } from './ui/button';
 
 
 function DeleteDocument() {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
+    const [isPending, startTransition] = useTransition();
+    const path = usePathname();
+
+
+    const handleDelete = async () => {
+        const roomId = path.split('/').pop();
+
+        if(!roomId) return;
+
+        console.log(roomId, 'roomid')
+    
+
+    }
   return (
-            <Dialog>
-  <DialogTrigger>Delete</DialogTrigger>
-  <DialogContent className='bg-red-700'>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
+            <Dialog open={isOpen} onOpenChange={setIsOpen}>
+                <Button asChild variant='destructive'>
+                      <DialogTrigger>Delete</DialogTrigger>
+                </Button>
+              
+                <DialogContent>
+                        <DialogHeader>
+                             <DialogTitle>Are you absolutely sure you want to Delete?</DialogTitle>
+                             <DialogDescription>
+                                 This action cannot be undone. This will permanently delete the document
+                                 and remove all users from the document.
+                             </DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter className='sm:justify-end gap-3'>
+                            <Button type='button' variant='destructive' onClick={handleDelete} disabled={isPending}>{isPending ? 'Deleting...' : 'Delete'}</Button>
+                             
+                        </DialogFooter>
+                 </DialogContent>
+            </Dialog>
 
   )
 }
